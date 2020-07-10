2b0e0c1534f25b47995b0a0e05249c37a100a49e
WebDesignForDio <khanghvse140097@fpt.edu.vn>
2020-07-10 08:24:51 +0700

    1st: the first upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to khanghv repository
+This repo contains the source code of a smple Java application used to demonstrate the CI - Continuous Integration process - a part of Introduction to Software Engineering course at FPT University- HCMC campus
+
+## You will find in this repo:
+* MathUtil class - holds the list of math methods
+* Test class - demonstrate on Unit Testing and TDD
+* .gitignore
+* Markdown file - this file itself
+
+## Connect me via
+1. [GitHub](https://github.com/WebDesignForDio)
+2. [Facebook](https://www.facebook.com/huavinhkhangfd3600)
+
+#### © 2020 by khanghv
---
+++ src/khanghv/util/MathUtil.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package khanghv.util;
+
+/**
+ *
+ * @author USER
+ */
+//đây là class chứa các hàm toán học tính toán với mục tiêu để xài chung cho các nơi khác, phàm cái gì mang tính chất xài chung, ta sẽ dùng static
+public class MathUtil {
+    //tính n gia thừa: 1*  2 * 3 * 4 .... --> tràn miền Integer (2tỷ 1)---> xài long ---> xài từ 15 trở lại
+
+    public static long computeFactorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+}
