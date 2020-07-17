7397279ae54dd9554fa047c77b20f2a6cda6e513
WebDesignForDio <khanghvse140097@fpt.edu.vn>
2020-07-17 08:25:32 +0700

    3rd: add tdd

--- src/mathutil/MathUtil.java
+++ src/mathutil/MathUtil.java
@@ -15,7 +15,18 @@ public class MathUtil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //hi vọng hàm trả dìa 120 nếu tính 5!
+        
         System.out.println("5! = " + computeFactorial(5));
+        
+        //hi vọng hàm trả về 1 nếu tính 1 !
+        System.out.println("1! = " + computeFactorial(1));
+        
+        //hi vọng hàm trả về 1 nếu tính  0!
+        System.out.println("0! = " + computeFactorial(0));
+        
+        //expected Exception nếu -5!
+        System.out.println("-5! = " + computeFactorial(-5));
     }
     
 }
