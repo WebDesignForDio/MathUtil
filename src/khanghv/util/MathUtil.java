f4ce4b2c9ff944d3190dc8960ad0c2896d6ed443
WebDesignForDio <khanghvse140097@fpt.edu.vn>
2020-07-20 08:31:51 +0700

    9th: full Exception Error Handler

--- src/khanghv/util/MathUtil.java
+++ src/khanghv/util/MathUtil.java
@@ -14,7 +14,7 @@ public class MathUtil {
     //tính n gia thừa: 1*  2 * 3 * 4 .... --> tràn miền Integer (2tỷ 1)---> xài long ---> xài từ 15 trở lại
 
     public static long computeFactorial(int n) {
-        if(n < 0)
+        if(n < 0 || n > 15)
             throw new IllegalArgumentException("Can not calculate the negative number. n must be >= 0");
         long result = 1;
         for (int i = 1; i <= n; i++) {
