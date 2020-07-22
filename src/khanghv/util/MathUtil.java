1aa74da6db61aea6d706c70c287a0c708272f7c3
WebDesignForDio <khanghvse140097@fpt.edu.vn>
2020-07-22 08:12:33 +0700

    Refactor the cF method, use the recursion technique new

--- src/khanghv/util/MathUtil.java
+++ src/khanghv/util/MathUtil.java
@@ -14,6 +14,10 @@ public class MathUtil {
     //tính n gia thừa: 1*  2 * 3 * 4 .... --> tràn miền Integer (2tỷ 1)---> xài long ---> xài từ 15 trở lại
 
     public static long computeFactorial(int n) {
+        if(n < 0 || n > 15)
+        {
+            throw new IllegalArgumentException("Invalid number, must be above 0 and below 16");
+        }
         if (n == 0 || n == 1) {
             return 1; //điều kiện dừng nhen
         }
